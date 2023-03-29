a3e48c0aa75ae26163b19eabbe04b9693c493960
swapan chetri <75736019+swapan007@users.noreply.github.com>
2023-03-29 12:39:12 +0530

    Add files via upload

---
+++ 6th sem_Compiler Design/EXPERIMENT 3.cpp
@@ -0,0 +1,35 @@
+/*
+experiment-3
+Write a C program to test whether a given identifier is valid or not.
+*/
+#include<stdio.h>
+//#include<conio.h>
+#include<ctype.h>
+int main()
+{
+    char a[10];
+    int flag,
+    i=1;
+//    clrscr();
+    printf("\n Enter an identifier:");
+    gets(a);
+    if(isalpha(a[0]))
+    flag=1;
+    else
+    printf("\n Not a valid identifier");
+    while(a[i]!='\0')
+    {
+        if(!isdigit(a[i])&&!isalpha(a[i]))
+        {
+            flag=0; break;
+           
+        } i++;
+       
+    }
+    if(flag==1)
+    printf("\n Valid identifier");
+//    getch();
+    return 0;
+   
+}
+
